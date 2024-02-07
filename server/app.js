f2adf0d90a64203f6237979b6f81805875d4e766
KarynaStep <stepanova.carina98@gmail.com>
2024-02-07 17:31:11 +0200

    Add node-schedule

--- server/app.js
+++ server/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const router = require('./routes');
 const handleErrors = require('./handleErrors');
+const schedule = require('node-schedule');
+const request = require('request');
+
+
 
 const app = express();
 app.use(cors());
@@ -14,4 +18,17 @@ app.use(handleErrors);
 
 module.exports = app;
 
-
+const job = schedule.scheduleJob('30 * * * *', function () {
+  request(
+    { uri: 'http://localhost:3000/api/rooms', method: 'DELETE' },
+    function (error, response, body) {
+      console.log(body);
+    }
+  );
+  request(
+    { uri: 'http://localhost:3000/api/users', method: 'DELETE' },
+    function (error, response, body) {
+      console.log(body);
+    }
+  );
+});
